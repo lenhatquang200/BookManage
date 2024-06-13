1a94fb54785947d54159bb8d184a39a78d958766
Q <nhatquang2106qh@gmail.com>
2024-06-13 10:50:55 +0700

    Add project files.

---
+++ BookManage/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManage
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public Book() {
+            this.Title = "Default title";
+            this.Author = "Default Author";
+            this.Description = "Default Description";
+        }
+        public Book(string title, string description, string author) {
+            this.Title = title;
+            this.Description = description;
+            this.Author = author;
+        }
+
+    }
+}
